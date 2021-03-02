893352b456b99a1a84bfed127a35361f2952bba9
amiramandaa <52105312+amiramandaa@users.noreply.github.com>
2021-03-02 10:59:07 +0700

    Update jumlah_a.cpp

--- jumlah_a.cpp
+++ jumlah_a.cpp
@@ -3,13 +3,18 @@ using namespace std;
 
 int main()
 {
-	int x= 5;
-	int y= 5;
+	int x;
+	int y;
 
-	cout<<" Penjumlahn x dan y "<<endl;
-	cout<<"x = "<<x<<endl;
-	cout<<"y = "<<y<<endl;
+	cout<<" Operasi Perhitungan x dan y "<<endl;
+	cout<<"Nilai x = "<<endl;
+	cin>>x;
+	cout<<"Nilai y = "<<endl;
+	cin>>y;
 	cout"\n";
 
 	cout<<"Hasil Penjumlahan = "<<x+y<<;
+	cout<<"Hasil Pengurangan = "<<x-y<<;
+	cout<<"Hasil Perkalian = "<<x*y<<;
+	cout<<"Hasil Pembagian = "<<x/y<<;
 }
